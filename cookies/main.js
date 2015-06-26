76a7ebe646c17356bfbe67de4de4f98079a594f8
Haojian Wu <hokein.wu@gmail.com>
2015-06-26 11:53:27 +0800

    Add cookies API sample.

---
+++ cookies/main.js
@@ -0,0 +1,9 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadUrl('file://' + __dirname + '/manager.html');
+});
+
