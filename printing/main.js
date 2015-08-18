af0208498f8a14e7150e91d10fa79971c2306b28
Haojian Wu <hokein.wu@gmail.com>
2015-08-18 14:19:21 +0800

    Close all window if the controling window is closed.

--- printing/main.js
+++ printing/main.js
@@ -3,6 +3,13 @@ var BrowserWindow = require('browser-window');
 
 var mainWindow = null;
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({x:100, y:100, width: 400, height: 380});
+  mainWindow = new BrowserWindow({x:100, y:100, width: 400, height: 420});
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.on('close', function() {
+    var windows = BrowserWindow.getAllWindows();
+    for (var i = 0; i < windows.length; ++i) {
+      if (windows[i] != mainWindow)
+        windows[i].close();
+    }
+  })
 });
