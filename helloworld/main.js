51560f044086b83764ad2e555b8dd601051c0426
Benjamin Henrion <zoobab@gmail.com>
2016-02-08 15:33:40 +0100

    add a simplest helloworld

---
+++ helloworld/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+
+    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+});
