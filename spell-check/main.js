316d8fa1fb5db4b4ee40f591cae7a718812e3837
Haojian Wu <hokein.wu@gmail.com>
2016-05-21 21:51:51 +0200

    :arrow_up: spell-check.

---
+++ spell-check/main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+// This method will be called when Electron has done everything
+// initialization and ready for creating browser windows.
+app.on('ready', function() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
