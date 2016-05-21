9b1254c548556f1420929364bf5b0c602a0dbb45
Haojian Wu <hokein.wu@gmail.com>
2016-05-21 17:25:17 +0200

    :arrow_up: helloworld.

--- helloworld/main.js
+++ helloworld/main.js
@@ -1,15 +1,12 @@
-'use strict';
+const {app, BrowserWindow} = require('electron');
 
-var app = require('app');
-var BrowserWindow = require('browser-window');
+let mainWindow;
 
-var mainWindow = null;
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+      height: 600,
+      width: 800
+  });
 
-app.on('ready', function() {
-    mainWindow = new BrowserWindow({
-        height: 600,
-        width: 800
-    });
-
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 });
