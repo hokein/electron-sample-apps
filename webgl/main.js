0d007ff921366aaf36457063183eda4d4aa74d8a
Haojian Wu <hokein.wu@gmail.com>
2016-05-21 21:45:07 +0200

    :arrow_up: webgl

--- webgl/main.js
+++ webgl/main.js
@@ -1,12 +1,6 @@
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
+const {app, BrowserWindow} = require('electron');
 
-// Report crashes to our server.
-require('crash-reporter').start();
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the javascript object is GCed.
-var mainWindow = null;
+let mainWindow;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
@@ -21,7 +15,7 @@ app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 800, height: 600, frame:false});
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
