90d057c6d733837e273bfc9306bb5c0ecc62f86d
Haojian Wu <hokein.wu@gmail.com>
2016-05-22 09:00:56 +0200

    :arrow_up: crash-reporter.

--- crash-report/main.js
+++ crash-report/main.js
@@ -1,8 +1,7 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
-var http = require('http');
-var crashReporter = require('crash-reporter');
-crashReporter.start({submitUrl: 'http://127.0.0.1:9999'});
+const {app, BrowserWindow, crashReporter} = require('electron');
+const http = require('http');
+
+crashReporter.start({submitURL: 'http://127.0.0.1:9999', companyName: 'sample'});
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -20,7 +19,7 @@ var mainWindow = null;
 app.on('ready', function() {
   server.listen(9999, '127.0.0.1', function () {
     mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadUrl('file://' + __dirname + '/index.html');
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
     mainWindow.toggleDevTools();
   });
 });
