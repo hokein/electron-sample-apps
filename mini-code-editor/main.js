1ff55fc6837b11561656e6a1d0927e03e5d4a612
successful_fella <preetjuneja@zingalbox.com>
2020-09-05 13:38:18 +0530

    enableRemoteModule set to true, which is by default false

--- mini-code-editor/main.js
+++ mini-code-editor/main.js
@@ -12,7 +12,7 @@ app.on('window-all-closed', function() {
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {nodeIntegration: true}});
+  mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {nodeIntegration: true, enableRemoteModule: true}});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
